1bc10862b5b34947fb33084df84e52eb7713029f
Ivan Yv <steel-97@mail.ru>
2026-02-11 04:03:30 +0300

    fix(nats): docs
    Signed-off-by: Ivan Yv <steel-97@mail.ru>

--- src/SlimMessageBus.Host.Nats/Config/NatsHandlerBuilderExtensions.cs
+++ src/SlimMessageBus.Host.Nats/Config/NatsHandlerBuilderExtensions.cs
@@ -5,8 +5,8 @@ public static class NatsHandlerBuilderExtensions
     /// <summary>
     /// Configure queue name that incoming requests (<see cref="TRequest"/>) are expected on.
     /// </summary>
-    /// <param name="builder"></param>
-    /// <param name="queue">Queue name</param>
+    /// <param name="natsBuilder"></param>
+    /// <param name="natsQueue">Queue name</param>
     /// <returns></returns>
     public static HandlerBuilder<TRequest, TResponse> Queue<TRequest, TResponse>(this HandlerBuilder<TRequest, TResponse> natsBuilder, string natsQueue)
     {
