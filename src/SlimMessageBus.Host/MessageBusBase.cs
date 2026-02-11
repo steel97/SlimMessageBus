36dcce77881795a0eccdb3a6c305886d94e4899e
Ivan Yv <steel-97@mail.ru>
2026-02-11 06:36:32 +0300

    fix attempt

--- src/SlimMessageBus.Host/MessageBusBase.cs
+++ src/SlimMessageBus.Host/MessageBusBase.cs
@@ -259,8 +259,8 @@ public abstract partial class MessageBusBase : IDisposable, IAsyncDisposable,
 
     protected internal virtual Task OnStart(string method = "unknown") => Task.CompletedTask;
     protected internal virtual Task OnStop(string method = "unknown") => Task.CompletedTask;
-    protected internal virtual Task OnStart() => Task.CompletedTask;
-    protected internal virtual Task OnStop() => Task.CompletedTask;
+    protected internal virtual Task OnStart() => this.OnStart("unknown");
+    protected internal virtual Task OnStop() => this.OnStop("unknown");
 
     protected void AssertActive()
     {
